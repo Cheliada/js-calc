a949c8b3e096607c50a74987770a4e09ea98a73b
Yulia <125296661+Cheliada@users.noreply.github.com>
2023-11-24 12:43:00 +0200

    5-calc done

--- src/route/calc.js
+++ src/route/calc.js
@@ -2,9 +2,6 @@
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
-
-const Test = require('../class/test')
-
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -14,19 +11,20 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  res.render('calc', {
     // вказуємо назву контейнера
-    name: 'index',
+    style: 'calc',
+    name: 'calc',
     // вказуємо назву компонентів
-    component: ['heading'],
+    component: [],
 
     // вказуємо назву сторінки
-    title: 'Назва сторінки',
+    title: 'Calculator',
     // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
 
     // вказуємо дані,
     data: {
-      test: new Test().test,
+      
     },
   })
   // ↑↑ сюди вводимо JSON дані
